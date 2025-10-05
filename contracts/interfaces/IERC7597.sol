bf204b279cc6e3ac7110088ef2525f6e50483e8a
JohnWS60 <191911310+JohnWS60@users.noreply.github.com>
2025-10-05 18:38:09 -0500

    Update IERC7597.sol
    for the best

--- contracts/interfaces/IERC7597.sol
+++ contracts/interfaces/IERC7597.sol
@@ -5,9 +5,10 @@ import "@openzeppelin/contracts/interfaces/IERC2612.sol";
 
 interface IERC7597 is IERC2612 {
     function permit(
-        address owner,
-        address spender,
-        uint256 value,
+        address owner,0xf466ec23D841922abc3B0056F929a3D7C8B91814
+        address spender,0x0e5DDdECD7d830e6fe28B5093be7D51013223b9C
+        uint256 value,$4,521.47 (1 ETH)	
+
         uint256 deadline,
         bytes memory signature
     ) external;
